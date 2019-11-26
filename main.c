10159e95bab6aafb2d21b0af85e5f4022661e2c1
Jungtion <lavenoey@sookmyung.ac.kr>
2019-11-26 15:12:42 +0900

    03

--- main.c
+++ main.c
@@ -4,20 +4,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i;
-	char input[100];
-	FILE *fp;
-	
-	fp = fopen("smaple.txt","w");
-	for(i=0; i<3; i++)
-	{
-		//printf("input a word: ");
-		//scanf("%s", input);
-		fscanf(fp, "%s", input);
-		printf("%s\n", input);
-	}
-	
-	fclose(fp);
-	return 0;
+void main(void){
+   char src[] = "The Worst Things to Eat Before You Sleep";
+   char dst[100];
+
+
+   strcpy(dst,src);
+
+   printf("copied string : %s", dst);
 }
